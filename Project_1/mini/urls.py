3207ae65837f0f5ca9fa42a7dec4269abb2563ec
gsw2019 <wsg@arizona.edu>
2026-02-20 17:28:58 -0700

    fix project structure

--- Project_1/mini/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.welcome, name="welcome"),
-    path("game_page/", views.game_page, name="game_page"),
-]
--- entry/wsgi.py
+++ entry/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Project_1 project.
+WSGI config for entry project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Project_1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'entry.settings')
 
 application = get_wsgi_application()
 
